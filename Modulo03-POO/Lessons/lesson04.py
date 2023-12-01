c04cb8a953b77e7e1cbdb7cb5094c9bdca0b826e
AdrianoR85 <adrianorosa085@gmail.com>
2023-12-01 15:42:25 -0300

    Maintaining states

--- Modulo03-POO/Lessons/lesson04.py
+++ Modulo03-POO/Lessons/lesson04.py
@@ -1,16 +1,30 @@
 # Maintaining states within the class
 
 class Camera:
-  def __init__(self, name, filmShooting=False):
+  def __init__(self, name, recording=False):
     self.name = name
-    self.filmShooting = filmShooting
+    self.recording = recording
     
-  def film(self):
-    print(f'{self.name} is film shooting...')
-    self.filmShooting = True
+  def record(self):
+    if self.recording:
+      print(f'{self.name} has already being recorded')
+      return
+    
+    print(f'{self.name} is recording...')
+    self.recording = True
+    
+  def stop(self):
+    if not self.recording:
+      print(f'{self.name} is not recording')
+      return
+    
+    print(f'{self.name} is stopping...')  
+    
+    
     
 c1 = Camera('Canon')
 c2 = Camera('Sony')
-c1.film()
-print(c1.filmShooting)
-print(c2.filmShooting)
+c1.record()
+c1.stop()
+print(c1.recording)
+print(c2.recording)
